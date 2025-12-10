9c860d8b666e6f507b1d1f0c38cbf2445417b19b
Ahmad Syauqi Albana <syauqiragi@gmail.com>
2025-12-10 15:51:19 +0000

    update popup

--- sdk.js
+++ sdk.js
@@ -20,6 +20,7 @@
         }
 
         const state = Math.random().toString(36).slice(2);
+
         const authUrl =
           this._config.authBaseUrl +
           "?redirect_uri=" +
@@ -27,26 +28,50 @@
           "&state=" +
           encodeURIComponent(state);
 
-        const w = 420;
-        const h = 600;
-        const left = window.screenX + (window.innerWidth - w) / 2;
-        const top = window.screenY + (window.innerHeight - h) / 2;
-
-        this._loginWindow = window.open(
-          authUrl,
-          "fakeauth_login",
-          `width=${w},height=${h},left=${left},top=${top}`
-        );
-
-        if (!this._loginWindow) {
-          return reject(new Error("Popup diblokir. Izinkan popup untuk melanjutkan."));
-        }
-
+        // simpan state dan resolver
         this._expectedState = state;
         this._loginResolver = resolve;
         this._loginRejecter = reject;
+
+        // redirect TANPA popup
+        window.location.href = authUrl;
       });
     },
+
+    // login() {
+    //   return new Promise((resolve, reject) => {
+    //     if (!this._config) {
+    //       return reject(new Error("FakeAuth belum di-init"));
+    //     }
+
+    //     const state = Math.random().toString(36).slice(2);
+    //     const authUrl =
+    //       this._config.authBaseUrl +
+    //       "?redirect_uri=" +
+    //       encodeURIComponent(this._config.redirectUri) +
+    //       "&state=" +
+    //       encodeURIComponent(state);
+
+    //     const w = 420;
+    //     const h = 600;
+    //     const left = window.screenX + (window.innerWidth - w) / 2;
+    //     const top = window.screenY + (window.innerHeight - h) / 2;
+
+    //     this._loginWindow = window.open(
+    //       authUrl,
+    //       "fakeauth_login",
+    //       `width=${w},height=${h},left=${left},top=${top}`
+    //     );
+
+    //     if (!this._loginWindow) {
+    //       return reject(new Error("Popup diblokir. Izinkan popup untuk melanjutkan."));
+    //     }
+
+    //     this._expectedState = state;
+    //     this._loginResolver = resolve;
+    //     this._loginRejecter = reject;
+    //   });
+    // },
 
     _handleMessage(event) {
       const data = event.data;
